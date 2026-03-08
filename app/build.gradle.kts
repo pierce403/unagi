422aa68ed74c17a8d77766d49ff4e2bf25e4ddd8
Dean Pierce <pierce403@gmail.com>
2026-03-08 12:18:36 -0700

    UX overhaul and ninja branding improvements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     applicationId = "ninja.unagi"
     minSdk = 24
     targetSdk = 35
-    versionCode = 24
-    versionName = "0.2.14"
+    versionCode = 25
+    versionName = "0.2.15"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
