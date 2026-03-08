34305f8bbb4b764f05dba8305fff8fa493fdb018
Dean Pierce <pierce403@gmail.com>
2026-03-08 10:54:49 -0700

    Release 0.2.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     applicationId = "ninja.unagi"
     minSdk = 24
     targetSdk = 35
-    versionCode = 18
-    versionName = "0.2.8"
+    versionCode = 19
+    versionName = "0.2.9"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
